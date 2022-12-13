2e0454bdc4f46a23f19b07ebba06f3d9d884484c
Arthur Kazemi <bidadh@gmail.com>
2022-12-13 22:14:57 +1100

    initial commit

---
+++ src/main/kotlin/com/webhookie/springintegrationmethodargissue/SpringIntegrationMethodArgIssueApplication.kt
@@ -0,0 +1,11 @@
+package com.webhookie.springintegrationmethodargissue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringIntegrationMethodArgIssueApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringIntegrationMethodArgIssueApplication>(*args)
+}
---
+++ src/test/kotlin/com/webhookie/springintegrationmethodargissue/SpringIntegrationMethodArgIssueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.webhookie.springintegrationmethodargissue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringIntegrationMethodArgIssueApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
